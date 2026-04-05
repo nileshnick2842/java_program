64fe161f7414263039ad967f984d54c7026c63f3
Nilesh Tiwari <nileshnick2842@gmail.com>
2026-04-05 09:51:40 +0530

    config file added and ADD functionality of docker being implanted

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM eclipse-temurin:17
 WORKDIR /app
 COPY helloworld.java .
 RUN javac helloworld.java
+ADD https://raw.githubusercontent.com/nileshnick2842/java_program/master/config.txt /app/config.txt
 EXPOSE 8080
-CMD ["java","helloworld"]
+CMD ["java", "helloworld"]
+
--- helloworld.java
+++ helloworld.java
@@ -1,14 +1,25 @@
 import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
 import java.io.*;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class helloworld {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", exchange -> {
-            String response = "<html><body><h1>Hello from Nilesh's Java Docker container!</h1><p>RUN instruction compiled this code during build!</p></body></html>";
+            String config = "";
+            try {
+                config = new String(Files.readAllBytes(Paths.get("/app/config.txt")));
+            } catch (Exception e) {
+                config = "Config not found";
+            }
+            String response = "<html><body>" +
+                "<h1>Hello from Nilesh's Java Docker container!</h1>" +
+                "<p>Config loaded via ADD instruction:</p>" +
+                "<pre>" + config + "</pre>" +
+                "</body></html>";
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
